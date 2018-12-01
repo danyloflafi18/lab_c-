87b3c1da9939df5e6ba5332253cb97ab656aa74e
Danylo Chalyi <d.chalyi16@gmail.com>
2018-12-02 01:38:18 +0200

    Clean code

---
+++ Source.c
@@ -0,0 +1,68 @@
+#include<stdio.h>
+#include<math.h>
+void sort(int a[], int size);
+void sort_matrix(int a[5][5]);
+void main()
+{
+	int i, j;
+	int a[5][5];
+	for (i = 0; i < 5; i++)
+	{
+		for (j = 0; j < 5; j++)
+		{
+			a[i][j] = rand() % 20; 
+			printf("a[%d][%d] = %d\n ", i + 1, j + 1, a[i][j]);
+
+		}
+		printf("\n");
+	}
+	printf("\nold matrix\n");
+	for (i = 0; i < 5; i++)
+	{
+		for (j = 0; j < 5; j++)
+			printf("%5d", a[i][j]);
+		printf("\n");
+	}
+	sort_matrix(a);
+	printf("\nnew matrix\n");
+	for (i = 0; i < 5; i++)
+	{
+		printf("[%d]:", i);
+		for (j = 0; j < 5; j++)
+		{
+			printf("%5d", a[i][j]);
+		}
+		printf("\n");
+	}
+	int theSumOfTheArithmeticMean = 0;
+
+	for (int j = 5 - 1; j > 0; j--)
+	{
+		int productItemsInEachColumn = 1;
+		for (int i = 5 - j; i < 5; i++)
+		{
+			productItemsInEachColumn *= a[i][j];
+		}
+		printf("<<%d>>\n", productItemsInEachColumn);
+		theSumOfTheArithmeticMean += productItemsInEachColumn;
+	}
+	printf("%f", theSumOfTheArithmeticMean / (5 - 1.0));
+	getchar();
+}
+void sort(int a[], int size)
+{
+	for (int i = 0; i < size; i++)
+		for (int j = 0; j < size; j++)
+			if (a[i] < a[j])
+			{
+				int temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
+			}
+}
+void sort_matrix(int a[][5])
+{
+	for (int i = 0; i < 5; i++)
+		sort(a[i], 5);
+}
+
